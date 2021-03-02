d953675e1d93b0b52eb4e8f6e0d1cb0ce6af0fb0
alvaro <alvaroarpal@gmail.com>
2021-03-02 19:29:29 +0100

    02/03 19:29 --WIP

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,14 +5,20 @@ import { LoginComponent } from './login/login.component';
 import { ProductListComponent } from './product-list/product-list.component';
 import { PruebaFormComponent } from './prueba-form/prueba-form.component';
 import { WelcomeComponent } from './welcome/welcome.component';
+import {HomeComponent} from './home/home.component';
 
 const routes: Routes = [
   { path: 'welcome', component: WelcomeComponent },
   { path: 'list', component: ProductListComponent },
   { path: 'login', component: LoginComponent },
-  { path: 'form' ,component: PruebaFormComponent},
+  { path: 'form' , component: PruebaFormComponent},
   { path: '', redirectTo: 'welcome', pathMatch: 'full' },
   { path: '**', redirectTo: 'welcome', pathMatch: 'full' },
+  { path: '', component: HomeComponent, canActivate: [AuthGuard] },
+  { path: 'login', component: LoginComponent },
+
+  // otherwise redirect to home
+  { path: '**', redirectTo: '' }
 ];
 
 @NgModule({
--- src/app/prueba-form/prueba-form.component.ts
+++ src/app/prueba-form/prueba-form.component.ts
@@ -1,32 +1,32 @@
-import { Component, OnInit } from "@angular/core";
-import { FormArray, FormBuilder, Validators } from "@angular/forms";
-import { IProducte } from "../interfaces/iproducte";
-import { DadesProductesService } from "../service/dades-productes.service";
+import { Component, OnInit } from '@angular/core';
+import { FormArray, FormBuilder, Validators } from '@angular/forms';
+import { IProducte } from '../interfaces/iproducte';
+import { DadesProductesService } from '../service/dades-productes.service';
 import {
   HttpClient,
   HttpClientModule,
   HttpHeaders,
-} from "@angular/common/http";
-import { Router } from "@angular/router";
-import { ICategoria } from "../interfaces/icategoria";
+} from '@angular/common/http';
+import { Router } from '@angular/router';
+import { ICategoria } from '../interfaces/icategoria';
 
 @Component({
-  selector: "app-prueba-form",
-  templateUrl: "./prueba-form.component.html",
-  styleUrls: ["./prueba-form.component.css"],
+  selector: 'app-prueba-form',
+  templateUrl: './prueba-form.component.html',
+  styleUrls: ['./prueba-form.component.css'],
 })
 export class PruebaFormComponent implements OnInit {
-  profileForm:any;
-  categories :ICategoria[]=[];
-  errorMessage: string = '';
-  
+  profileForm: any;
+  categories: ICategoria[] = [];
+  errorMessage = '';
+
   constructor(
     private fb: FormBuilder,
     private producteService: DadesProductesService,
     private httpClient: HttpClient,
     private router: Router
   ) {
-    
+
   }
   ngOnInit(): void {
     this.producteService.getCategories().subscribe(
@@ -35,18 +35,18 @@ export class PruebaFormComponent implements OnInit {
       },
       (error) => {
         this.errorMessage = error.message; // treurem l'error a html
-        
+
       }
     );
     this.profileForm = this.fb.group({
-      nom: ["", Validators.required],
-      descripcio: ["", Validators.required],
-      valoracio:["", Validators.required],
-      profile: ["", Validators.required],
-      categoria:["", Validators.required]
+      nom: ['', Validators.required],
+      descripcio: ['', Validators.required],
+      valoracio: ['', Validators.required],
+      profile: ['', Validators.required],
+      categoria: ['', Validators.required]
     });
   }
-  onFileSelect(event:any) {
+  onFileSelect(event: any) {
     if (event.target.files.length > 0) {
       const file = event.target.files[0];
       this.profileForm.get('profile').setValue(file);
@@ -54,10 +54,10 @@ export class PruebaFormComponent implements OnInit {
   }
   updateProfile() {
     this.profileForm.patchValue({
-      nom: "Cazadora",
-      descripcio: ":D",
-      categoria:7,
-        
+      nom: 'Cazadora',
+      descripcio: ':D',
+      categoria: 7,
+
     });
   }
 
@@ -75,7 +75,7 @@ export class PruebaFormComponent implements OnInit {
     };
   }*/
   onSubmit() {
-    console.log('intento submit')
+    console.log('intento submit');
     const formData = new FormData();
     formData.append('nom', this.profileForm.get('nom').value);
     formData.append('descripcio', this.profileForm.get('descripcio').value);
